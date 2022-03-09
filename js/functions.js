0f86e3613bd09ca7f5d521ba44dc798afe477789
anooshiravan-G1 <hanosheravan@yahoo.com>
2022-03-10 02:05:07 +0330

    add signUp Form

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,15 @@
+//appearance
 $(document).ready(function(){
     $(".nav-item.dropdown").click(function(){
         $(".dropdown-menu").slideToggle("slow");
     })
 })
+
+
+
+
+// Data
+function submitLoginFrm(){
+    var formData = JSON.stringify($("#logForm").serializeArray());
+    console.log(formData);
+}
